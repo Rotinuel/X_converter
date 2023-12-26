5012d901f4d45ff0ca973c0a16e0794bbd6cf44f
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2023-12-26 16:55:35 +0100

    first commit

---
+++ lib/x_converter_material_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class XConverterMaterialPage extends StatelessWidget{
+  const XConverterMaterialPage({super.key});
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: Center(child: Text('Hello, senade!!!'),
+      )
+    );
+  }
+}
